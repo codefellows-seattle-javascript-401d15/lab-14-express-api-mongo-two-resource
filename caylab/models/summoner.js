5d0055a30725b9db75780c8944ffd63c2a478283
Caleb <caleb.wells1@gmail.com>
2017-06-29 15:52:25 -0700

    removed comments in summoner model

--- caylab/models/summoner.js
+++ caylab/models/summoner.js
@@ -12,40 +12,31 @@ const summonerSchema = Schema({
 })
 
 const Summoner = module.exports = mongoose.model('summoner', summonerSchema)
-//chaining .then() is a good way to work asynchronously, and make it do multiple things
 Summoner.findByIdAndAddMinion = function(id, minion){
-  return Summoner.findById(id)//find a specific pet
+  return Summoner.findById(id)
   .then(summoner => {
-    minion.summonerId = summoner._id//we're assigning the id of the pet to the person's  id
-    this.tempSummoner = summoner //this is equal to the list returned from the db
-    return new Minion(minion).save() //return the new Person after you save it to the db
-  })//the data that we use for the next .then() is the data that was returned from the previous .then()
+    minion.summonerId = summoner._id
+    this.tempSummoner = summoner
+    return new Minion(minion).save()
+  })
   .then(minion => {
-    this.tempSummoner.minions.push(minion._id) //this is pushing this to *this* schema's array of ids
+    this.tempSummoner.minions.push(minion._id)
     this.tempMinion = minion
     return this.tempSummoner.save()
   })
   .then(() => this.tempMinion)
 }
 Summoner.findByIdAndRemoveMinion = function(id, minion){
-  return Summoner.findById(id)//find a specific pet
+  return Summoner.findById(id)
   .then(summoner => {
-    minion.summonerId = summoner._id//we're assigning the id of the pet to the person's  id
-    this.tempSummoner = summoner //this is equal to the list returned from the db
-    return new Minion(minion).save() //return the new Person after you save it to the db
-  })//the data that we use for the next .then() is the data that was returned from the previous .then()
+    minion.summonerId = summoner._id
+    this.tempSummoner = summoner
+    return new Minion(minion).save()
+  })
   .then(minion => {
-    this.tempSummoner.minions.pop(minion._id) //this is pushing this to *this* schema's array of ids
+    this.tempSummoner.minions.pop(minion._id)
     this.tempMinion = minion
     return this.tempSummoner.save()
   })
   .then(() => this.tempMinion)
 }
-
-// person => req.body = {
-//   name: 'some name',
-//   content: 'some content',
-//   petId: 'some pet._id'
-// }
-//when you make a request from the database, it gives you a copy of that data,
-//we are then modifying it, then overwriting it with .save()
